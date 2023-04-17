5a23f07ea616ccf9adc2067ace27eae3702f5c46
Maxv1z <meitstudying@gmail.com>
2023-04-17 10:32:01 +0300

    Edding email.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,36 @@
-import React from 'react' 
+import React, {useRef} from 'react' 
 import './FormStyles.css'
+import emailJs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailJs.sendForm(
+      import.meta.env.REACT_APP_SERVICE_ID,
+      import.meta.env.REACT_APP_TEMPLATE_ID,
+      form.current,
+      import.meta.env.REACT_APP_USER_ID
+    ).then(
+      result => console.log(result.text), 
+      error => console.log(error.text)
+    );
+
+    e.target.reset();
+  };
+
   return (
     <div className='form'>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Your name</label>
-        <input type='text'></input>
+        <input type='text' name='user_name' required/>
         <label>Email</label>
-        <input type='text'></input>
-        <label>Subject</label>
-        <input type='text'></input>
-        <label>Details</label>
-        <textarea rows='6' placeholder='Type a short message here'/>
-        <button className='btn'>Submit</button>
+        <input type='email' name='user_email' required/>
+        <label>Message</label>
+        <textarea name="message" placeholder='Type a short message here'/>
+        <input type="submit" value="Send" className='btn'/>
       </form>
     </div>
   )
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import HeroImg from '../components/HeroImg'
 import Navbar from '../components/Navbar'
-import Form from '../components/Form'
+/*import Form from '../components/Form'*/
 import Footer from '../components/Footer'
+import '../components/FormStyles.css'
+
+import emailJs from "@emailjs/browser";
+
+const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+// eslint-disable-next-line
+    emailJs.sendForm(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
+      form.current,
+      process.env.REACT_APP_USER_ID
+    ).then(
+      result => console.log(result.text), 
+      error => console.log(error.text)
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <div className='form'>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Your name</label>
+        <input type='text' name='user_name' required/>
+        <label>Email</label>
+        <input type='email' name='user_email' required/>
+        <label>Message</label>
+        <textarea name="message" placeholder='Type a short message here'/>
+        <input type="submit" value="Send" className='btn'/>
+      </form>
+    </div>
+  )
+};
 
 const Contact = () => {
   return (
